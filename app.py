2e3b305b4bf719d48be5f53b2fe25981189de362
Athacyr87 <souza.87.ferreira@gmail.com>
2025-06-22 13:53:12 -0300

    Atualiza app.py e index.html com template HTML

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Olá, Flask no GitHub - teste aqui!'
+    return render_template('index.html')
 
 @app.route('/buscador')
 def buscar():
