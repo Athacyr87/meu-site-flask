491a677b898328978ba6df30d917eb50ad39e7e5
Athacyr87 <souza.87.ferreira@gmail.com>
2025-06-22 13:36:21 -0300

    Commit correto agora

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return 'Olá, Flask no GitHub!'
 
+@app.route('/buscador')
+def buscar():
+    return 'Aqui é um buscador'
+
 if __name__ == '__main__':
     app.run(debug=True)
