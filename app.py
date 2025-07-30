daf4b24f83b3e6a68c14bc1a139b17c5f2ec7aea
Athacyr87 <souza.87.ferreira@gmail.com>
2025-07-30 20:58:00 -0300

    Adiciona nova funcionalidade ao projeto

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-import os
+
 app = Flask(__name__)
 
 
 # Configurar o banco de dados SQLite
-
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///exemplo.db')  # Fallback para SQLite local
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///exemplo.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Inicializar o SQLAlchemy
